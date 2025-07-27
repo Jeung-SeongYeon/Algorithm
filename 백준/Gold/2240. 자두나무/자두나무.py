93d421493a4a366c0b018315ac62310bf5e76ea0
QuiteEasilyDone <123040115+Jeung-SeongYeon@users.noreply.github.com>
2025-07-27 15:01:52 +0900

    [Gold IV] Title: 자두나무, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+T, W = map(int, input().split(" "))
+
+tree = [0]
+for i in range(T):
+    tree.append(int(input()))
+
+dp = [[0] * (W+1) for i in range(T+1)]
+
+for i in range(T+1):
+    if (tree[i] == 1):
+        dp[i][0] = dp[i-1][0] + 1
+    else:
+        dp[i][0] = dp[i-1][0]
+    for j in range(1, W+1):
+
+        if (tree[i] == 1 and j % 2 == 0) or (tree[i] == 2 and j % 2 == 1):
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + 1
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j])
+
+print(max(dp[T]))
